e4084f1129b946aef06c6650285f52bc4b186c5c
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-02 17:18:00 +0300

    alert prompt

---
+++ javascript/console_alert_prompt.js
@@ -0,0 +1,17 @@
+console.error ('Yanlış işlem!!!');
+console.warn ('Uyarı: Şifre çok kısa...');
+//tüm konsolu temizler.
+console.clear ();
+//konsolda tablo oluşturur.
+console.table ({isim: 'Hasan', yas: 23});
+//araki geçen süreyi yazdırır.
+console.time ('id');
+console.console.log ('Merhaba');
+console.timeEnd ('id');
+
+//veri alma.
+
+alert ('Dikkat');
+
+let girilenSayi = prompt ('Bir sayı giriniz..');
+console.log (girilenSayi);
