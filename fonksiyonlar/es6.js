028e14858bf06c9eaff53a8f02666051f92ca08f
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-04 18:21:40 +0300

    arrangement

---
+++ fonksiyonlar/es6.js
@@ -0,0 +1,10 @@
+const karesi = sayi => {
+  return console.log (sayi * sayi);
+};
+
+karesi (10);
+
+let arabalar = ['BMW', 'Mercades', 'Pagani'];
+
+console.log (arabalar);
+console.log (arabalar.toString ());
