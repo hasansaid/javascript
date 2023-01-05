367b835001de59e27e6a2a32cb9284698d696975
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-05 19:52:08 +0300

    adding

---
+++ array/global_local_shadowed.js
@@ -0,0 +1,26 @@
+let isim = "Emre";
+
+selamVer();
+
+function selamVer() {
+    let yas = 30;
+    console.log("Merhaba " + isim + " yaş:" + yas);
+}
+
+let favoriRengim = "Yeşil";
+favoriRenginiSoyle(favoriRengim);
+
+function favoriRenginiSoyle(renk) {
+
+    let favoriRengim = "Sarı";
+    console.log("Favori rengim " + favoriRengim);
+}
+console.log(favoriRengim);
+
+
+
+
+
+let isim2 = "Hasan";
+let yas = 40;
+console.log("Yaş :" + yas);
