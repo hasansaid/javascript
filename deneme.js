c1c9a0f913a4fae672b748dd492b74189ce04f48
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-05 18:44:58 +0300

    arrangement

--- deneme.js
+++ deneme.js
@@ -1,22 +1,50 @@
-let sehirler = ['Ankara', 'İstanbul', 'İzmir', 'Bursa', 'Malatya'];
-console.log (sehirler.toString ());
-console.log (sehirler.join (' - '));
-console.clear ();
-a = sehirler.push ('Adana');
-a2 = sehirler.push ('Manisa');
-b = sehirler.length;
-console.log (sehirler.toString ());
-console.log (sehirler.join (' - '), a, a2, b);
+// let sehirler = ['Ankara', 'İstanbul', 'İzmir', 'Bursa', 'Malatya'];
+// console.log (sehirler.toString ());
+// console.log (sehirler.join (' - '));
+// console.clear ();
+// a = sehirler.push ('Adana');
+// a2 = sehirler.push ('Manisa');
+// b = sehirler.length;
+// console.log (sehirler.toString ());
+// console.log (sehirler.join (' - '), a, a2, b);
 
-sehirler.pop (); // son elemanı yok eder
-console.log (sehirler);
-console.clear ();
+// sehirler.pop (); // son elemanı yok eder
+// console.log (sehirler);
+// console.clear ();
 
-sehirler.shift (); // ilk elemanı yok eder
-console.log (sehirler);
+// sehirler.shift (); // ilk elemanı yok eder
+// console.log (sehirler);
 
-sehirler.unshift ('Gaziantep'); //ilk eleman olarak ekler
-console.log (sehirler);
-console.clear ();
-sehirler.splice (6, 0, 'Alo', 'fırat', 654, 45, 85);
-console.log (sehirler);
+// sehirler.unshift ('Gaziantep'); //ilk eleman olarak ekler
+// console.log (sehirler);
+// console.clear ();
+// sehirler.splice (6, 0, 'Alo', 'fırat', 654, 45, 85);
+// console.log (sehirler);
+// console.clear ();
+
+// let insan = {
+//   isim: 'Hasan',
+//   yas: '23',
+//   evliMi: false,
+// };
+
+// console.log (insan.isim);
+// console.log (typeof insan);
+// console.log (insan instanceof Object);
+
+// let renkler = ["kırmızı"]
+// console.log(renkler[0])
+// console.log(typeof renkler)
+// console.log(renkler instanceof Array)
+
+let myDizi = [1, 2, 3];
+
+const diziyiKoplayaveIkiileCarp = dizi => {
+  let geciciDizi = [];
+  for (let i = 0; i < dizi.length; i++) {
+    geciciDizi[i] = dizi[i] * 2;
+  }
+  return console.log (geciciDizi);
+};
+
+diziyiKoplayaveIkiileCarp (myDizi);
