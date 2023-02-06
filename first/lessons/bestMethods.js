0fecdeb71c9997646f9081c0fbe2c07a47e6c01c
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-02-06 16:06:35 +0300

    adding

--- first/lessons/bestMethods.js
+++ first/lessons/bestMethods.js
@@ -115,3 +115,83 @@ console.log (points5);
 // shift : En baştaki elemanı siler. Bütün elemanlar bir index sola kaydırıldığı için baştaki eleman kaybolur.
 points5.shift ();
 console.log (points5);
+
+// findIndex : Elemanın index'ini bulur.
+const number = [70, 75, 25, 35, 10, 80, 27, 49, 56];
+
+const arrayIndex = number.findIndex (point => point == 35);
+number[arrayIndex] = 50;
+console.log (number);
+
+const people = [
+  {name: 'hasan', point: 48},
+  {name: 'can', point: 90},
+  {name: 'kaya', point: 43},
+  {name: 'ali', point: 52},
+];
+
+const objectIndex = people.findIndex (s => s.name == 'ali');
+people[objectIndex].name = 'Hakan';
+console.log (people[3]);
+
+// sort : Sıralama için kullanılır.
+// reverse : Tersten sıralama için kullanılır.
+const stringSıralama = ['can', 'ali', 'hasam', 'buse', 'zühra', 'ahmet'];
+stringSıralama.sort ();
+console.log (stringSıralama);
+// Alfabetik sıralama yapar.
+
+stringSıralama.reverse ();
+console.log (stringSıralama);
+// Tersten alfabetik sıralama yapar.
+
+const numberSıralama = [60, 84, 0, , 0.5, 85, 3, 8, 9, 65, 10, 100, 18, 48, 35];
+numberSıralama.sort ();
+console.log (numberSıralama);
+// İlk hanesine göre sıralama yapar.
+
+numberSıralama.sort ((a, b) => b - a);
+console.log (numberSıralama);
+// Büyükten küçüğe sıralama yapar.
+
+const objectSıralama = [
+  {name: 'hasan', point: 48},
+  {name: 'can', point: 90},
+  {name: 'kaya', point: 43},
+  {name: 'ali', point: 52},
+];
+
+objectSıralama.sort ((a, b) => b.point - a.point);
+console.log (objectSıralama);
+
+console.clear ();
+
+const students4 = [
+  {name: 'hasan', point: 48},
+  {name: 'can', point: 90},
+  {name: 'kaya', point: 43},
+  {name: 'ali', point: 52},
+  {name: 'ayşe', point: 85},
+  {name: 'fuat', point: 37},
+  {name: 'hüsnü', point: 23},
+  {name: 'alpy', point: 76},
+];
+
+const filtered = students4.filter (student => student.point < 50);
+
+const plusPoint = filtered.map (student => {
+  return `${student.name} adlı öğrencinin yeni notu: ${student.point + 15}`;
+});
+
+console.log (plusPoint);
+
+// Temiz kod..
+
+const newNotes = students4
+  .filter (student => student.point < 50)
+  .map (
+    student =>
+      `${student.name} adlı öğrencinin yeni notu: ${student.point + 15}`
+  );
+
+console.log (newNotes);
